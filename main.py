05a83551ac60419072a6b8ac6c5e43e45efcc102
exata0mente <exata0mente@gmail.com>
2023-04-25 19:07:38 -0300

    Atributos e métodos em Python

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from models import *
 
-michael_scott = Personagem("Michael Scott", "Gerente Regional")
-dwight_schrute = Personagem("Dwight Schrute", "Assistente Regional")
-jim_halpert = Personagem("Jim Halpert", "Vendedor")
-pam_beesly = Personagem("Pam Beesly", "Recepcionista")
+michael_scott = Personagem("Michael Scott", "Gerente Regional", 45, "Scranton", "That's what she said!")
+michael_scott.imprimir_info()
+
--- models.py
+++ models.py
@@ -1,4 +1,14 @@
 class Personagem:
-    def __init__(self, nome, cargo):
+    def __init__(self, nome, cargo, idade, escritorio, frase_famosa):
         self.nome = nome
         self.cargo = cargo
+        self.idade = idade
+        self.escritorio = escritorio,
+        self.frase_famosa = frase_famosa
+
+    def imprimir_info(self):
+        print("Nome:", self.nome)
+        print("Cargo:", self.cargo)
+        print("Idade:", self.idade)
+        print("Escritório:", self.escritorio)
+        print("Frase Famosa:", self.frase_famosa)
